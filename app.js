acd113c33064627e9380474e77932d2ea0cad1d5
Felipe Duarte Vieira <lipao23@hotmail.com>
2025-10-27 20:21:15 -0300

    Refactor toggleMenu to include backdrop filter

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 function toggleMenu(){
     const sideMenu = document.getElementById("side-menu");
     const image = document.getElementById("iconHide");
+    const backdrop = document.getElementById("content")
+    
 
     if(sideMenu.style.width == "250px"){
         //close menu
         sideMenu.style.width = "0";
         image.style.display = "block";
-        sideMenu.style.backdropFilter = "blur(5px)";
+        backdrop.style.filter = "none";
     } else {
         //open the menu
         sideMenu.style.width = "250px";
         image.style.display = "none";
-        sideMenu.style.backdropFilter = "none";
+        backdrop.style.filter = "blur(5px)";
 
     }
 }
